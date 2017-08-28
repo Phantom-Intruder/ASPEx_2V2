035108c4287bc0275e4c3981ea49be04f1ec0dc0
Mewantha.Bandara <Mewantha.Bandara@SL1-L0007>
2017-08-28 17:07:40 +0530

    Added update for category

--- ASPEx_2/Controllers/HomeController.cs
+++ ASPEx_2/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace ASPEx_2.Controllers
 {
     public class HomeController : Controller
     {
+        public static int IDNew                           = 0;
         public ActionResult Index()
         {
             return View();
@@ -17,42 +18,60 @@ namespace ASPEx_2.Controllers
 
         public ActionResult ProductList()
         {
-            ViewBag.Message     = "Your application description page.";
-            ProductModels product = new ProductModels();
+            ViewBag.Message                     = "Your application description page.";
+            ProductModels product               = new ProductModels();
 
             return View(product);
         }
 
         public ActionResult CategoryList()
         {
-            ViewBag.Message     = "Your contact page.";
-            CategoryModels category = new CategoryModels();
+            ViewBag.Message                     = "Your contact page.";
+            CategoryModels category             = new CategoryModels();
             return View(category);
         }
 
         public ActionResult EditCategoryView()
         {
-            ViewBag.Message     = "Your application description page.";
+            ViewBag.Message                     = "Your application description page.";
             return View();
         }
 
         [HttpPost]
-        public ActionResult EditCategoryView(string nameField, string descriptionField, string filePathField)
+        public ActionResult EditCategoryView(string nameField, string descriptionField, string filePathField, string isEditField)
         {
-            ViewBag.Message     = "Added " + nameField + descriptionField + " " + filePathField;
-
-            Category record     = Category.ExecuteCreate(nameField, descriptionField, filePathField, 1, 50, 51);
-            record.Insert();
+            if (isEditField == "false")
+            {
+                Category record                 = Category.ExecuteCreate(nameField, descriptionField, filePathField, 1, 50, 51);
+                record.Insert();
+            }
+            else
+            {
+                //TODO: Updating
+                Category record                 = Category.ExecuteCreate(nameField, descriptionField, filePathField, 1, 50, 51);
+                record.Update(IDNew, record);
+            }
             return PartialView("_AddedView");
         }
 
         [HttpPost]
-        public ActionResult EditProductView(string nameField, string descriptionField, string filePathField, string priceField, string categoryField)
+        public ActionResult EditProductView(string nameField, string descriptionField, string filePathField, string priceField, string categoryField, string isEditField)
         {
-            ViewBag.Message     = "Added " + nameField + descriptionField + " " + filePathField + " " + priceField + " " + categoryField ;
+           if (isEditField == "false")
+            {
+                ViewBag.Message                 = "Added " + nameField + descriptionField + " " + filePathField + " " + priceField + " " + categoryField;
 
-            Product record      = Product.ExecuteCreate(Int32.Parse(categoryField), nameField, descriptionField, Decimal.Parse(priceField), filePathField, 1, 50, 51);
-            record.Insert();
+                Product record                  = Product.ExecuteCreate(Int32.Parse(categoryField),
+                                                                        nameField,
+                                                                        descriptionField,
+                                                                        Decimal.Parse(priceField),
+                                                                        filePathField, 1, 50, 51);
+                record.Insert();
+            }
+            else
+            {
+                //TODO: updating
+            }
             return PartialView("_AddedView");
         }
 
@@ -61,12 +80,13 @@ namespace ASPEx_2.Controllers
         {
             try
             {
-                Product product     = Product.ExecuteCreate(Int32.Parse(id));
-                ViewBag.Message     = "Added " + product.Name+ " " + product.Description;
-                ViewBag.Name        = product.Name;
-                ViewBag.Description = product.Description;
-                ViewBag.Price       = product.Price;
-                ViewBag.FileName    = product.ImageName;
+                Product product                 = Product.ExecuteCreate(Int32.Parse(id));
+                
+                ViewBag.Message                 = "Added " + product.Name+ " " + product.Description;
+                ViewBag.Name                    = product.Name;
+                ViewBag.Description             = product.Description;
+                ViewBag.Price                   = product.Price;
+                ViewBag.FileName                = product.ImageName;
             }catch (ArgumentNullException n)
             {
                 return View();
@@ -79,10 +99,12 @@ namespace ASPEx_2.Controllers
         {
             try
             {
-                Category category     = Category.ExecuteCreate(Int32.Parse(id));
-                ViewBag.Name        = category.Name;
-                ViewBag.Description = category.Description;
-                ViewBag.FileName    = category.ImageName;
+                IDNew                           = Int32.Parse(id);
+            
+                Category category               = Category.ExecuteCreate(Int32.Parse(id));
+                ViewBag.Name                    = category.Name;
+                ViewBag.Description             = category.Description;
+                ViewBag.FileName                = category.ImageName;
             }
             catch (ArgumentNullException n)
             {
@@ -93,7 +115,7 @@ namespace ASPEx_2.Controllers
 
         public ActionResult EditProductView()
         {
-            ViewBag.Message     = "Your application description page.";
+            ViewBag.Message                     = "Your application description page.";
 
             return View();
         }
